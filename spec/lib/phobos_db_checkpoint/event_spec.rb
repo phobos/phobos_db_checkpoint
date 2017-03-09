3956948e9bd776e099ade2f5319ce55fab00e33e
Mathias Klippinge <mathias.klippinge@gmail.com>
2017-03-09 10:57:06 +0100

    More readable test

--- spec/lib/phobos_db_checkpoint/event_spec.rb
+++ spec/lib/phobos_db_checkpoint/event_spec.rb
@@ -68,16 +68,20 @@ describe PhobosDBCheckpoint::Event, type: :db do
   describe '.order_by_event_time_and_created_at' do
     before do
       PhobosDBCheckpoint::Event.delete_all
-      PhobosDBCheckpoint::Event.create(id: '1', entity_id: '1', created_at: Time.now-100, event_time: nil)
-      PhobosDBCheckpoint::Event.create(id: '2', entity_id: '2', created_at: Time.now-200, event_time: nil)
-      PhobosDBCheckpoint::Event.create(id: '3', entity_id: '3', created_at: Time.now-300, event_time: Time.now-300)
+      PhobosDBCheckpoint::Event.create(id: '1', entity_id: '1', event_time: nil, created_at: Time.now-100, )
+      PhobosDBCheckpoint::Event.create(id: '2', entity_id: '2', event_time: nil, created_at: Time.now-200)
+      PhobosDBCheckpoint::Event.create(id: '3', entity_id: '3', event_time: Time.now-300, created_at: Time.now-300)
       PhobosDBCheckpoint::Event.create(id: '4', entity_id: '4', event_time: nil).tap { |r| r.created_at=nil; r.save! }
       PhobosDBCheckpoint::Event.create(id: '5', entity_id: '5', event_time: Time.now-400).tap { |r| r.created_at=nil; r.save! }
       PhobosDBCheckpoint::Event.create(id: '6', entity_id: '6', event_time: Time.now-500).tap { |r| r.created_at=nil; r.save! }
     end
 
-    it 'sorts it in ascending order leaving null timestamp records trailing at the end' do
-      expect(PhobosDBCheckpoint::Event.order_by_event_time_and_created_at.pluck('entity_id')).to eq ['3', '5', '6', '1', '2', '4']
+    it 'sorts it in descending order leaving null timestamp records trailing at the end' do
+      expect(
+        PhobosDBCheckpoint::Event
+          .order_by_event_time_and_created_at
+          .pluck('entity_id')
+      ).to eq ['3', '5', '6', '1', '2', '4']
     end
   end
 end
--- spec/lib/phobos_db_checkpoint/failure_spec.rb
+++ spec/lib/phobos_db_checkpoint/failure_spec.rb
@@ -252,16 +252,20 @@ describe PhobosDBCheckpoint::Failure, type: :db do
   describe '.order_by_event_time_and_created_at' do
     before do
       PhobosDBCheckpoint::Failure.delete_all
-      PhobosDBCheckpoint::Failure.create(id: '1', entity_id: '1', created_at: Time.now-100, event_time: nil)
-      PhobosDBCheckpoint::Failure.create(id: '2', entity_id: '2', created_at: Time.now-200, event_time: nil)
-      PhobosDBCheckpoint::Failure.create(id: '3', entity_id: '3', created_at: Time.now-300, event_time: Time.now-300)
+      PhobosDBCheckpoint::Failure.create(id: '1', entity_id: '1', event_time: nil, created_at: Time.now-100)
+      PhobosDBCheckpoint::Failure.create(id: '2', entity_id: '2', event_time: nil, created_at: Time.now-200)
+      PhobosDBCheckpoint::Failure.create(id: '3', entity_id: '3', event_time: Time.now-300, created_at: Time.now-300)
       PhobosDBCheckpoint::Failure.create(id: '4', entity_id: '4', event_time: nil).tap { |r| r.created_at=nil; r.save! }
       PhobosDBCheckpoint::Failure.create(id: '5', entity_id: '5', event_time: Time.now-400).tap { |r| r.created_at=nil; r.save! }
       PhobosDBCheckpoint::Failure.create(id: '6', entity_id: '6', event_time: Time.now-500).tap { |r| r.created_at=nil; r.save! }
     end
 
-    it 'sorts it in ascending order leaving null timestamp records trailing at the end' do
-      expect(PhobosDBCheckpoint::Failure.order_by_event_time_and_created_at.pluck('entity_id')).to eq ['3', '5', '6', '1', '2', '4']
+    it 'sorts it in descending order leaving null timestamp records trailing at the end' do
+      expect(
+        PhobosDBCheckpoint::Failure
+          .order_by_event_time_and_created_at
+          .pluck('entity_id')
+      ).to eq ['3', '5', '6', '1', '2', '4']
     end
   end
 end
